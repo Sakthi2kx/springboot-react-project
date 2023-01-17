b22fc6a9046606a7c9a4bf1ce392d1c403d506bc
Sakthi2kx <110442883+Sakthi2kx@users.noreply.github.com>
2023-01-17 08:25:14 +0530

    Update constant.js

--- polling-app-client/src/constants/constant.js
+++ polling-app-client/src/constants/constant.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api' || 'http://127.0.0.1:8080/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8081/api' ||'http://localhost:8081/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_QUESTION_MAX_LENGTH = 140;
